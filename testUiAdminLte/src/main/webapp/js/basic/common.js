29776286ec8908ac7f1a64127478e4359bed93e6
lujian <apple60588@vip.qq.com>
2018-07-12 09:35:16 +0800

    1

--- testUiAdminLte/src/main/webapp/js/basic/common.js
+++ testUiAdminLte/src/main/webapp/js/basic/common.js
@@ -35,7 +35,6 @@ function Map() {
     	for(var i=0;i<keys.length;i++) {
     		if(keys[i] == key) {
     			this.keys.baoremove(i);
-    			alert("success");
     		}
     	}
 //    	this.keys.remove(key);     
