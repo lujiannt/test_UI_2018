681c1d7d5d1793527a6397cc3648087e785f83e9
lujian <apple60588@vip.qq.com>
2018-07-28 15:43:14 +0800

    1

--- testUi_SpringMvc/src/main/webapp/js/app/hello/hello.js
+++ testUi_SpringMvc/src/main/webapp/js/app/hello/hello.js
@@ -1,9 +1,11 @@
 require(["app/basic/basic", "helloEX"], function (basic, helloEX) {
     console.log("hello!");
     
+    //test require module
     var basicJs = basic.basicJs;
     basicJs.showHello();
     
+    //test require module (no AMD)
     showHelloEx();
     helloEX.test1();
 })
